2493a09b296725f93dc12218c48ebe2b2bff5328
chanweiyan <chanweiyan007@gmail.com>
2026-05-22 21:41:25 +0800

    fix: implement user entity

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,33 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { authPlugins } from 'mysql2';
+import { UserModule } from './user/user.module';
+import { User } from './user/entities/user.entity';
 
 @Module({
-  imports: [],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: "mysql",
+      host: "localhost",
+      port: 3306,
+      username: "root",
+      password: "Cwy17824",
+      database: "following_test",
+      synchronize: true,
+      logging: true,
+      entities: [User],
+      poolSize: 10,
+      extra: {
+        authPlugin: {
+          sha256_password: authPlugins.sha256_password,
+        },
+      }
+    }),
+    UserModule
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
-export class AppModule {}
+export class AppModule { }
